69756e1d6c41a6b37aa2b0c29fc6b20ee08753fc
Jasmine <fancheny@cn.ibm.com>
2023-06-12 23:10:10 +0800

    脚手架基本写法

--- fan-test/bin/index.js
+++ fan-test/bin/index.js
@@ -1,33 +1,61 @@
 #!/usr/bin/env node
+const yargs = require('yargs/yargs')
+const {hideBin} = require('yargs/helpers')
+const dedent = require('dedent')
+const pkg = require("../package.json");
 
-const lib = require('fan-test-lib')
-const arg = require('process').argv
-console.log(arg)
-const command = arg[2]
-// console.log(command)
-const options = arg.slice(3)
-console.log(options)
-if (options.length>1) {
-    let [option, param] = options
-    option = option.replace('--', '')
-    console.log(option, param)
+const arg = hideBin(process.argv)
+const cli = yargs(arg)
+const argv = process.argv.slice(2)
 
-    if (command) {
-        if (lib[command]) {
-            lib[command]({ option, param })
-        } else {
-            console.log('无效的命令')
-        }
-    } else {
-        console.log('请输入命令')
+const context = {
+  fanVersion: pkg.version,
+};
+cli
+    .usage('Usage: imooc-test [command] <options>')
+    .demandCommand(1, "A command is required. Pass --help to see all available commands and options.")
+    .recommendCommands()
+    .fail((err,msg)=>{
+      console.log(err)
+    })
+    .strict()
+    .alias("h", "help")
+    .alias("v", "version")
+    .wrap(cli.terminalWidth())
+    .epilogue(dedent`
+    When a command fails, all logs are written to lerna-debug.log in the current working directory.
+
+    For more information, check out the docs at https://lerna.js.org/docs/introduction
+  `)
+  .options({
+    debug:{
+        type:'boolean',
+        describe:'Bootstrap debug mode',
+        alias:'d'
     }
-}
-if(command.startsWith('--')||command.startsWith('-')){
-    const globalOption = command.replace(/--|-/g,'')
-    console.log(globalOption)
-    if(globalOption==='version'||globalOption ==='V'){
-        console.log('1.1.1')
-    }
-}
-
-// console.log("welcome to fan test!!!, now success")
+  })
+  .option('registry',{
+    type:'string',
+    describe:'Define registry',
+    alias:'r'
+  })
+  .group(['debug'],'Dev options')
+  .group(['registry'],'Extra options')
+  .command('init [name]', 'Do init a project', (yargs) => {
+    return yargs
+      .option('name', {
+        describe: 'Name of a project',
+        type:'string',
+        alias:'n'
+      })
+  }, (argv) => {
+    console.log(argv)
+  })
+  .command({
+    command:'list',
+    aliases: ["ls", "la", "ll"],
+    describe: "List local packages",
+    builder:(yargs)=>{},
+    handler:(argv)=>{console.log(argv)}
+  })
+  .parse(argv, context)
